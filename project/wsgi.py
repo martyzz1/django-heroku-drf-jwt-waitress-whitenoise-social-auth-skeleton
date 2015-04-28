516db009b2a314d5872a8dbe5a320589f6c4528c
Martyzz1 <martin@sharehood.co.uk>
2015-04-28 17:25:47 +0100

    Setup django-configurations properly http://django-configurations.readthedocs.org/en/latest/#quickstart

--- project/wsgi.py
+++ project/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
