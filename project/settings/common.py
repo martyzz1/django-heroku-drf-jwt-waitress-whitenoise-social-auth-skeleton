d8109a7028c65e3481357a3b528fb9334690cc44
Martyzz1 <martin@sharehood.co.uk>
2015-04-29 12:08:51 +0100

    add some JWT config defaults

--- project/settings/common.py
+++ project/settings/common.py
@@ -270,4 +270,38 @@ class Common(Configuration):
     }
     # END django-rest-framework CONFIGURATION
 
+    # django-rest-framework-jwt CONFIGURATION
+    # showing all defaults for info, we'll want to override one or two later
+    JWT_AUTH = {
+        'JWT_ENCODE_HANDLER':
+        'rest_framework_jwt.utils.jwt_encode_handler',
+
+        'JWT_DECODE_HANDLER':
+        'rest_framework_jwt.utils.jwt_decode_handler',
+
+        'JWT_PAYLOAD_HANDLER':
+        'rest_framework_jwt.utils.jwt_payload_handler',
+
+        'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+        'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+        'JWT_RESPONSE_PAYLOAD_HANDLER':
+        'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+        # 'JWT_SECRET_KEY': settings.SECRET_KEY,
+        'JWT_ALGORITHM': 'HS256',
+        'JWT_VERIFY': True,
+        'JWT_VERIFY_EXPIRATION': True,
+        'JWT_LEEWAY': 0,
+        # 'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+        'JWT_AUDIENCE': None,
+        'JWT_ISSUER': None,
+
+        'JWT_ALLOW_REFRESH': False,
+        # 'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+        'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    }
+    # END django-rest-framework-jwt CONFIGURATION
+
     # Your common stuff: Below this line define 3rd party libary settings
