628935444a9d86fe3c079b5ecb2a331dad85c637
Martyzz1 <martin@sharehood.co.uk>
2015-05-08 11:14:10 +0100

    Update admin for users

--- users/admin.py
+++ users/admin.py
@@ -8,8 +8,8 @@ class UserAdmin(EmailUserAdmin):
     """
     You can customize the interface of your model here.
     """
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_display = ('email', 'first_name', 'last_name', 'user_type', 'is_staff')
+    list_filter = ('user_type', 'is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email', 'first_name', 'last_name')
+    ordering = ('email', 'first_name', 'last_name', 'user_type')
     filter_horizontal = ('groups', 'user_permissions',)
